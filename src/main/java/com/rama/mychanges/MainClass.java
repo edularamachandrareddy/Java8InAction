31e971d0aa1875fb61a3bb4d875273fe0e249a6f
ESWARAMMA <ursramachandrareddy@gmail.com>
2016-10-05 02:13:26 +0530

    spaces removed

--- src/main/java/com/rama/mychanges/MainClass.java
+++ src/main/java/com/rama/mychanges/MainClass.java
@@ -2,16 +2,12 @@ package com.rama.mychanges;
 
 import java.util.stream.Stream;
 import static java.util.stream.Collectors.toList;
-
 import java.util.List;
 
 public class MainClass {
-
 	public static void main(String[] args) {
-		Stream<String> stream=Stream.of("rama","chandra","reddy");
-	List<String>	list=stream.filter(s -> s.length()>=5).collect(toList());
-		
-System.out.println(list);
+		Stream<String> stream = Stream.of("rama", "chandra", "reddy");
+		List<String> list = stream.filter(s -> s.length() >= 5).collect(toList());
+		System.out.println(list);
 	}
-
 }
